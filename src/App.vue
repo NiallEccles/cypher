79b053fe59ba1fc977d94fe9c558efffd00c7bea
Niall Eccles <nialle@rothco.ie>
2021-08-20 16:55:33 +0100

    Start work on password generator.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Password msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Password from './components/Password.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Password
+  }
+})
+</script>
+
+<style>
+@font-face {
+  font-family: PlusJakartaSans-Medium;
+  src: url('./assets/fonts/PlusJakartaSans-Medium.woff2');
+}
+#app {
+  font-family: PlusJakartaSans-Medium;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Password.vue
@@ -0,0 +1,106 @@
+<template>
+  <fieldset>
+    <div class="fieldset-item">
+      <div class="input-stack">
+        <label for="password-length" id="password-length" aria-hidden="true">
+          Length
+        </label>
+        <div class="password-length">
+          <span>{{ minLength }}</span>
+          <input
+            v-model="passwordLength"
+            name="password-length"
+            aria-labelledby="password-length"
+            type="range"
+            min="0"
+            max="100"
+            style="--track-fill: 30%"
+          />
+          <span>{{ maxLength }}</span>
+        </div>
+        <div class="fieldset-item">
+          <input
+            type="checkbox"
+            checked
+            id="text-notifications"
+            name="text-notifications"
+          />
+          <div class="input-stack">
+            <label for="text-notifications">
+              <h3>Include Symbols</h3>
+              <small
+                >Get notified about all text messages sent to your device</small
+              >
+            </label>
+          </div>
+        </div>
+        <br />
+        <input type="text" v-model="passwordLength" />
+      </div>
+    </div>
+  </fieldset>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "Password",
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  data: () => {
+    return {
+      minLength: 8,
+      passwordLength: 8,
+      maxLength: 100,
+    };
+  },
+  methods: {
+    changeSetting: () => {},
+  },
+  setup: () => {
+    const count = ref(0);
+    return { count };
+  },
+});
+
+// const sliders = document.querySelectorAll('input[type="range"]')
+
+// const rangeToPercent = (slider: unknown) => {
+//   const max = slider.getAttribute('max') || 10;
+//   const percent = slider.value / max * 100;
+
+//   return `${parseInt(percent)}%`
+// }
+
+// sliders.forEach(slider => {
+//   slider.style.setProperty('--track-fill', rangeToPercent(slider));
+
+//   slider.addEventListener('input', e => {
+//     e.target.style.setProperty('--track-fill', rangeToPercent(e.target));
+//   })
+// })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
